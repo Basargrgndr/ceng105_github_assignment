2858b6e2b1b846f87927b7f2b994d1a4be3125d1
Basargrgndr <basargrgndr@gmail.com>
2023-11-22 20:21:30 +0300

    Update ceng105.c
    ceng

--- ceng105.c
+++ ceng105.c
@@ -30,5 +30,15 @@ int main(){
     printf("SATIR TOPLAM\n");
 
 
-    return 0;
+for(i=0; i<satir; i++){
+		satirToplam=0;
+		for(j=0; j<sutun;j++){
+sutunToplam=0;
+		for(i=0; i<satir;i++){
+			sutunToplam += matris[i][j];
+		}
+		printf("Matrisin %d. sutunundaki sayilarin toplami: %d\n",j+1,sutunToplam);	
+	}
+}
+return 0;
     }
