eec204228b4b7206b8b52285cea5621af5e2fb19
boranyzgc <109727601+boranyzgc@users.noreply.github.com>
2023-11-22 20:26:33 +0300

    Sütun
    ceng105

--- ceng105.c
+++ ceng105.c
@@ -40,5 +40,17 @@ sutunToplam=0;
 		printf("Matrisin %d. sutunundaki sayilarin toplami: %d\n",j+1,sutunToplam);	
 	}
 }
-return 0;
+
+	printf("SUTUN TOPLAM\n");
+
+	for(j=0; j<sutun; j++){
+		sutunToplam=0;
+		for(i=0; i<satir;i++){
+			sutunToplam += matris[i][j];
+		}
+		printf("Matrisin %d. sutunundaki sayilarin toplami: %d\n",j+1,sutunToplam);	
+	}
+	
+	return 0;
+
     }
